d98a6c0bc1d9b945196c9ed3fcc63cabf7eb25bc
Antoine <me@antoinebellanger.fr>
2025-01-10 09:41:28 +0100

    feat: Task 04 -> 05

--- Game.hs
+++ Game.hs
@@ -12,3 +12,21 @@ instance Show Item where
     show Sword = "sword"
     show Bow = "bow"
     show MagicWand = "magicWand" 
+
+data Mob = Mummy | Skeleton Item | Witch (Maybe Item)
+    deriving (Eq, Show)
+
+createMummy :: Mob
+createMummy = Mummy
+
+createArcher :: Mob
+createArcher = Skeleton Bow
+
+createKnight :: Mob
+createKnight = Skeleton Sword
+
+createWitch :: Mob
+createWitch = Witch Nothing
+
+createSorceress :: Mob
+createSorceress = Witch $ Just MagicWand
