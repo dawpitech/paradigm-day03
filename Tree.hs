b7f1ce859c4eabb7dfa0e5e422f528d2b96f73ad
Antoine <me@antoinebellanger.fr>
2025-01-10 11:48:38 +0100

    feat: Task 12

--- Tree.hs
+++ Tree.hs
@@ -7,3 +7,8 @@
 
 data Tree a = Empty | Node (Tree a) a (Tree a)
     deriving Show
+
+addInTree :: Ord a => a -> Tree a -> Tree a
+addInTree v Empty = Node Empty v Empty
+addInTree v (Node l ev r) | v < ev    = Node (addInTree v l) ev r
+                          | otherwise = Node l ev (addInTree v r)
